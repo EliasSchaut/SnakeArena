93f6ebc2707c369879a5a301465f0d015693c618
Dennis Kobert <dennis@kobert.dev>
2020-08-18 01:54:43 +0200

    Add run script

---
+++ run.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+top=$(git rev-parse --show-toplevel 2>/dev/null)
+if [ $? -ne 0 ]; then
+  echo "Not in a git repo!"
+  exit 1
+fi
+
+cd $top/src
+javac $(find . -name \*.java) && jar -cvfm test.jar mainfest.txt $(find . -name \*.class) && java -jar test.jar
